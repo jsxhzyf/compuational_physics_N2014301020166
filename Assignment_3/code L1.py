e998234599d5bc98731130e38cafafe8fc6a4e56
jsxhzyf <jsxhzyf@qq.com>
2016-09-24 19:47:12 +0800

    Update code L1.py

--- Assignment_3/code L1.py
+++ Assignment_3/code L1.py
@@ -5,11 +5,11 @@ import time
 times=input('Please input the times you want the name to move horiziontally')
 for y in range(times):
   for x in ['1','2','3','4','5','6','7','8']:
-    print d[x],'#####   #######'
-    print d[x],'   #       #'
-    print d[x],'  #        #'
-    print d[x],' #         #'
-    print d[x],'#####      #'    
+    print d[x],'#####   ##   ##   ######'
+    print d[x],'   #      # #     #      '
+    print d[x],'  #        #      ###### ' 
+    print d[x],' #         #      #      '
+    print d[x],'#####      #      #      '    
     print('\n')  
     time.sleep(0.3)    
   sys.stderr.write("\x1b[2J\x1b[H")
