6da1f62ab13b559c8869d3fd5755b2b6736abe3d
jsxhzyf <jsxhzyf@qq.com>
2016-09-25 23:13:12 +0800

    Update code L1.py

--- Assignment_3/code L1.py
+++ Assignment_3/code L1.py
@@ -1,15 +1,18 @@
-x='1'
-d={'1':'  '*1 ,'2':'    '*2,'3':'      '*3,'4':'        '*4,'5':'          '*4,'6':'            '*4,'7':'              '*4,'8':'                '*4}
-import sys
-import time
-times=input('Please input the times you want the name to move horiziontally')
-for y in range(times):
-  for x in ['1','2','3','4','5','6','7','8']:
-    print d[x],'#####   ##   ##   ######'
-    print d[x],'   #      # #     #      '
-    print d[x],'  #        #      ###### ' 
-    print d[x],' #         #      #      '
-    print d[x],'#####      #      #      '    
-    print('\n')  
-    time.sleep(0.3)    
-  sys.stderr.write("\x1b[2J\x1b[H")
+print('My name is Fei')
+x=int(input('input a number:'))
+for i in range(x):
+    print(i*' '+'#########             # ') 
+    print(i*' '+'#                       ') 
+    print(i*' '+'#            # #      # ') 
+    print(i*' '+'######### #      #    # ') 
+    print(i*' '+'#        #        #   # ') 
+    print(i*' '+'#        ##########   # ') 
+    print(i*' '+'#        #            # ') 
+    print(i*' '+'#         #       #   # ') 
+    print(i*' '+'#          # # # #    # ') 
+    if i==x-1:
+        break
+    import time
+    time.sleep(0.2)
+    import os
+    i=os.system('cls')
